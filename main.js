758d85e4e70140637fcea408df2f39c58632dcf0
hsakina20 <115486180+hsakina20@users.noreply.github.com>
2022-11-14 20:29:32 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+class Message{
+    constructor(author,text){
+        this.author=author;
+        this.time=new Date();
+        this.text=text
+    }
+    toString(){
+        return`${this.time.getHours()}:${this.time.getMinutes()} ${this.author}: ${this.text}`
+    }
+}
+class Messenger{
+    constructor(){
+        this.messages = []
+    }
+    show_history(){
+        this.messages.forEach(item=>console.log(item))
+    }
+    send(author,text){
+        const message =new Message(author,text)
+        this.messages.push(message.toString())
+    }
+}
+function gettime() {
+    let now = new Date();
+    return `${now.getHours()}:${now.getMinutes()}`
+}
+
+console.log(gettime())
+const first= new Messenger()
+first.send("Sakina","Salam necesen?")
+first.send("Zeyneb","Yaxsi sen?")
+first.send("Sakina","Mende")
+messenger.show_history();
